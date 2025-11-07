57c0f29282066da82ea9032e0fc546ed239ed816
desertengineer <102937158+desertengineer@users.noreply.github.com>
2025-11-08 01:08:03 +0200

    Update blog.js

--- 0. Blog/js/blog.js
+++ 0. Blog/js/blog.js
@@ -1,3 +1,20 @@
 function loadBlogData() {
-    importHTML("https://raw.githubusercontent.com/desertengineer/BekeiratSoftwareBlog/refs/heads/main/0.%20Blog/css/skinStyle.css", "#page-skin-1"); 
+    // Declaring 
+    const blogCssFolder = "https://raw.githubusercontent.com/desertengineer/BekeiratSoftwareBlog/refs/heads/main/0.%20Blog/css/";
+    const blogJsFolder = "https://raw.githubusercontent.com/desertengineer/BekeiratSoftwareBlog/refs/heads/main/0.%20Blog/js/";
+    const blogHtmlFolder = "https://raw.githubusercontent.com/desertengineer/BekeiratSoftwareBlog/refs/heads/main/0.%20Blog/htmls/";
+    const blogXmlFolder = "https://raw.githubusercontent.com/desertengineer/BekeiratSoftwareBlog/refs/heads/main/0.%20Blog/xmls/";
+    // File & Element id arrays
+    var cssArray = [["skinStyle.css", "#page-skin-1"]];
+    var jsArray = [];
+    var htmlArray = [];
+    var xmlsArray = [];
+
+    // Loop on data arrays
+    if (cssArray.length != 0) {
+        cssArray.forEach(element => {
+            importHTML(blogCssFolder + element[0], element[1]);
+        });
+    }
+
 }
