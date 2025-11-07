814ea3e21f97071fc94e86f81036609bec6a93f6
desertengineer <102937158+desertengineer@users.noreply.github.com>
2025-11-08 01:58:03 +0200

    dyn theme

--- 0. Blog/js/blog.js
+++ 0. Blog/js/blog.js
@@ -21,8 +21,8 @@ function loadBlogData() {
     const htmlData = {
         folder: "https://raw.githubusercontent.com/desertengineer/BekeiratSoftwareBlog/refs/heads/main/0.%20Blog/htmls/",
         data: [
-            // ["header.html", "#header"],
-            // ["footer.html", "#footer"]
+            // ["top-nav-section.html", ".top-nav-section"],
+            ["top-nav-section.html", ".top-nav-section"]
         ]
     };
 
